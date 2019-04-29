602d14f4957c676977d78daab32862080f213db0
root <root@web-server.us-east1-b.c.bold-site-226203.internal>
2019-04-29 04:02:12 +0000

    added new code

---
+++ 08-file-exists.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -f 01-helloworldjj.sh ]; then
+	echo "file exists"
+else
+	echo "file does not exists"
+fi
---
+++ 09-folder-exists.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -d ../shellscriptss ]; then
+	echo "folder exists"
+else
+	echo "folder does not exists"
+fi
